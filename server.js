533ba23b0cb94bee07dbcf8c65cc61b1a985c2e5
secsbox <wjexc@hotmail.com>
2025-06-11 01:31:09 -0700

    - debug network connection

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the Express server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
